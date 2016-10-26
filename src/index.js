2fd87a721b82c0434a0eac0406be69239fe4add7
tsvetkovpro <285019@gmail.com>
2016-10-26 21:36:27 +0500

    change structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './components/App';
+import './styles/index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/toastr/build/toastr.css';
 
 
 ReactDOM.render(
-<App />,
+	<App />,
 	document.getElementById('root')
 );
